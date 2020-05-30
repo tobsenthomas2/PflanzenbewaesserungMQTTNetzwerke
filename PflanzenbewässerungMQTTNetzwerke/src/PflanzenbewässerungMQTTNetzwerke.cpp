ccb147bdcdaf809a11accaee5c8293dc1eb97f4a
Tobi Thom <tobsenthomas2@web.de>
2020-05-30 16:13:02 +0200

    Projektdateien hinzufügen.

---
+++ PflanzenbewässerungMQTTNetzwerke/src/PflanzenbewässerungMQTTNetzwerke.cpp
@@ -0,0 +1,10 @@
+﻿/*
+ Name:		PflanzenbewässerungMQTTNetzwerke.cpp
+ Created:	30.05.2020 16:05:47
+ Author:	Admin
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "PflanzenbewässerungMQTTNetzwerke.h"
+
+
---
+++ PflanzenbewässerungMQTTNetzwerke/src/PflanzenbewässerungMQTTNetzwerke.h
@@ -0,0 +1,19 @@
+﻿/*
+ Name:		PflanzenbewässerungMQTTNetzwerke.h
+ Created:	30.05.2020 16:05:47
+ Author:	Admin
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _PflanzenbewässerungMQTTNetzwerke_h
+#define _PflanzenbewässerungMQTTNetzwerke_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
