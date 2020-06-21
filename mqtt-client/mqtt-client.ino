9f5e269312926bb2d04d3b2dabbe910fc3dded6e
Simon Kuhlmann <Simon.Kuhlmann@gmx.de>
2020-06-21 14:23:40 +0200

    wlan-passwort

--- mqtt-client/mqtt-client.ino
+++ mqtt-client/mqtt-client.ino
@@ -67,14 +67,14 @@ RTC_DATA_ATTR int bootCount = 0; //Zaehler fuer erfolge Neustarts durch DeepSlee
 
 // Hier Netzwerkdaten Eintragen
 
-//const char* ssid = "wlankuhl";
-//const char* password = "leer";
-//const char* mqtt_server = "192.168.10.58";
+const char* ssid = "wlankuhl";
+const char* password = "leer";
+const char* mqtt_server = "192.168.10.58";
 
 
-const char* ssid = "o2-WLAN80";
-const char* password = "49YMT8F7E84L8673";//4 3
-const char* mqtt_server = "192.168.178.57";
+//const char* ssid = "o2-WLAN80";
+//const char* password = "49YMT8F7E84L8673";//4 3
+//const char* mqtt_server = "192.168.178.57";
 
 
 
